bc513eecd13b24c0697aaf51cbf3ecb47f41cac5
Moha <mimohadinho@gmail.com>
2025-05-10 09:28:16 +0200

    Stage 2/4: Print an image of the animal

--- Zookeeper with Java/task/src/Main.java
+++ Zookeeper with Java/task/src/Main.java
@@ -1,11 +1,27 @@
 public class Main {
     public static void main(String[] args) {
+        String camel = """
+            Switching on the camera in the camel habitat...
+             ___.-''''-.
+            /___  @    |
+            ',,,,.     |         _.'''''''._
+                 '     |        /           \\
+                 |     \\    _.-'             \\
+                 |      '.-'                  '-.
+                 |                               ',
+                 |                                '',
+                  ',,-,                           ':;
+                       ',,| ;,,                 ,' ;;
+                          ! ; !'',,,',',,,,'!  ;   ;:
+                         : ;  ! !       ! ! ;  ;   :;
+                         ; ;   ! !      ! !  ; ;   ;,
+                        ; ;    ! !     ! !   ; ;
+                        ; ;    ! !    ! !     ; ;
+                       ;,,      !,!   !,!     ;,;
+                       /_I      L_I   L_I     /_I
+            Look at that! Our little camel is sunbathing!""";
+
         // write your code here
-        System.out.println("""
-                I love animals!
-                Let's check on the animals...
-                The deer looks fine.
-                The bat looks happy.
-                The lion looks healthy.""");
+        System.out.println(camel);
     }
 }
