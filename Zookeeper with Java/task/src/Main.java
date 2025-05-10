0b685fb3086ecb615bd86763cd90404ce5fcb996
Moha <mimohadinho@gmail.com>
2025-05-10 08:15:21 +0200

    my commit

---
+++ Zookeeper with Java/task/src/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+        // write your code here
+        System.out.println("""
+                I love animals!
+                Let's check on the animals...
+                The deer looks fine.
+                The bat looks happy.
+                The lion looks healthy.""");
+    }
+}
